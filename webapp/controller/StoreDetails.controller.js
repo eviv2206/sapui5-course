e64a787e6e0ea808ec0966114bd1e236b5a04ce0
eviv2206 <zhenya.saprin@mail.ru>
2024-01-07 23:06:16 +0300

    feat(hw-7): add search for products and stores, add sorting at products table

--- webapp/controller/StoreDetails.controller.js
+++ webapp/controller/StoreDetails.controller.js
@@ -1,18 +1,48 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], function (Controller, Filter, FilterOperator) {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+], function (Controller, JSONModel, Filter, FilterOperator, Sorter) {
     "use strict";
+
+    const SORT_NONE = "";
+    const SORT_ASC = "ASC";
+    const SORT_DESC = "DESC";
 
     return Controller.extend("yauheni.sapryn.controller.StoreDetails", {
 
         onInit: function () {
+
+            this._oView = this.getView();
+            this._oView.addEventDelegate({
+                onBeforeHide: function(oEvent) {
+                    this._clearSearch();
+                    this._clearAllSorting();
+                    this._clearFilters();
+                },
+            }, this)
+
             const oRouter = this.getOwnerComponent().getRouter();
             const oCurrentRoute = oRouter.getHashChanger().getHash();
             const oParameters = oRouter.getRouteInfoByHash(oCurrentRoute);
 
             this.getOwnerComponent().getModel("selectedIds").setProperty("/StoreID", oParameters.arguments.StoreID);
+
+            const oSortModel = new JSONModel({
+                Name: SORT_NONE,
+                Price: SORT_NONE,
+                Specs: SORT_NONE,
+                SupplierInfo: SORT_NONE,
+                MadeIn: SORT_NONE,
+                ProductionCompanyName: SORT_NONE,
+                Rating: SORT_NONE,
+            });
+
+            this.oSortModel = oSortModel;
+
+            this.getView().setModel(oSortModel, "products");
 
             oRouter.getRoute("StoreDetails").attachPatternMatched(this.onPatternMatched, this);
         },
@@ -49,28 +79,20 @@ sap.ui.define([
         },
 
         getProductsFilterCount: function (storeId, filterType, odataModel, onSuccess) {
-            let oFilter;
+            let oFilter = new Filter({
+                filters: [
+                    new Filter({
+                        path: "StoreId",
+                        operator: FilterOperator.EQ,
+                        value1: storeId
+                    })
+                ],
+            });
 
-            if (filterType === "") {
+            if (filterType !== "") {
                 oFilter = new Filter({
                     filters: [
-                        new Filter({
-                            path: "StoreId",
-                            operator: FilterOperator.EQ,
-                            value1: storeId
-                        })
-                    ],
-                });
-
-            } else {
-                oFilter = new Filter({
-                    filters: [
-                        new Filter({
-                            path: "StoreId",
-                            operator: FilterOperator.EQ,
-                            value1: storeId
-                        }),
-
+                        oFilter,
                         new Filter({
                             path: "Status",
                             operator: FilterOperator.EQ,
@@ -117,7 +139,160 @@ sap.ui.define([
                 StoreID: storeId,
                 ProductID: productId,
             });
-        }
+        },
 
+        onProductSearch: function (oEvent) {
+            const sSearchValue = oEvent.getParameter("query");
+            this.performProductSearch(sSearchValue);
+        },
+
+        performProductSearch: function (sSearchValue) {
+            const table = this.byId("ProductsTable");
+            const oODataModel = this.getView().getModel("odata");
+            oODataModel.metadataLoaded().then(() => {
+                let oFilter = this.createFilters(sSearchValue);
+                table.getBinding("items").filter(oFilter);
+            });
+        },
+
+        createFilters: function (sSearchValue) {
+            let oFilter = new Filter({
+                filters: [
+                    new Filter({
+                        path: "Name",
+                        operator: FilterOperator.Contains,
+                        value1: sSearchValue
+                    }),
+                    new Filter({
+                        path: "Specs",
+                        operator: FilterOperator.Contains,
+                        value1: sSearchValue
+                    }),
+                    new Filter({
+                        path: "SupplierInfo",
+                        operator: FilterOperator.Contains,
+                        value1: sSearchValue
+                    }),
+                    new Filter({
+                        path: "MadeIn",
+                        operator: FilterOperator.Contains,
+                        value1: sSearchValue
+                    }),
+                    new Filter({
+                        path: "ProductionCompanyName",
+                        operator: FilterOperator.Contains,
+                        value1: sSearchValue
+                    }),
+                ],
+                or: true,
+            });
+
+            if (+sSearchValue) {
+                oFilter = new Filter({
+                    filters: [
+                        oFilter,
+                        new Filter({
+                            path: "Price",
+                            operator: FilterOperator.EQ,
+                            value1: +sSearchValue
+                        }),
+                        new Filter({
+                            path: "Rating",
+                            operator: FilterOperator.EQ,
+                            value1: +sSearchValue
+                        })
+                    ],
+                    or: true,
+                })
+            }
+            return oFilter;
+        },
+
+        onSortButtonPress: function (oEvent) {
+            const oSortButton = oEvent.getSource();
+            const sSortBy = oSortButton.data("sortBy");
+            let sSortType = this.oSortModel.getProperty(`/${sSortBy}`);
+
+            this._clearAllSorting();
+
+            const oProductsTable = this.byId("ProductsTable");
+            const oItemsBinding = oProductsTable.getBinding("items");
+
+
+
+            switch (sSortType) {
+                case SORT_NONE: {
+                    sSortType = SORT_ASC;
+                    oSortButton.setIcon("sap-icon://sort-ascending");
+                    break;
+                }
+
+                case SORT_ASC: {
+                    sSortType = SORT_DESC;
+                    oSortButton.setIcon("sap-icon://sort-descending");
+                    break;
+                }
+
+                case SORT_DESC: {
+                    sSortType = SORT_NONE;
+                    oSortButton.setIcon("sap-icon://sort");
+                    break;
+                }
+            }
+
+            this.oSortModel.setProperty(`/${sSortBy}`, sSortType);
+
+            if (sSortType === SORT_ASC || sSortType === SORT_DESC) {
+                const bSortDesc = sSortType === SORT_DESC;
+                const oSorter = new Sorter(sSortBy, bSortDesc);
+                oSorter.fnCompare = this._compareFunction;
+                oItemsBinding.sort(oSorter);
+            }
+        },
+
+        _clearAllSorting: function (oEvent) {
+            const oTable = this.byId("ProductsTable");
+            oTable.getBinding("items").sort(null);
+
+            const oColumns = oTable.getColumns();
+            oColumns.forEach((column) => {
+                if (column.getAggregation("header")) {
+                    const oButton = column.getAggregation("header").getItems()[0];
+                    oButton.setIcon("sap-icon://sort");
+                }
+            });
+
+            const data = this.oSortModel.getData();
+
+            for (const key in data) {
+                this.oSortModel.setProperty(`/${key}`, SORT_NONE);
+            }
+        },
+
+        _clearSearch: function () {
+            const searchField = this.byId("ProductSearchField");
+            searchField.setValue("");
+        },
+
+        _clearFilters: function () {
+            const table = this.byId("ProductsTable");
+            table.getBinding("items").filter(null);
+        },
+
+        _compareFunction: function(value1, value2) {
+
+            if (typeof value1 == "string" && typeof value2 == "string") {
+                return value1.localeCompare(value2);
+            }
+
+            value2 = parseFloat(value2);
+
+            value1 = parseFloat(value1);
+
+            if (value1 < value2) return -1;
+            if (value1 === value2) return 0;
+            if (value1 > value2) return 1;
+
+        },
     });
 });
--- webapp/controller/StoresList.controller.js
+++ webapp/controller/StoresList.controller.js
@@ -1,9 +1,55 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function (Controller) {
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("yauheni.sapryn.controller.StoresList", {
+
+        onSearch: function (oEvent) {
+            const sSearchValue = oEvent.getParameter("query");
+            this.performStoreSearch(sSearchValue);
+        },
+
+        performStoreSearch: function (sSearchValue) {
+            const list = this.byId("StoresList");
+            const oODataModel = this.getView().getModel("odata");
+            oODataModel.metadataLoaded().then(() => {
+                let oFilter = new Filter({
+                    filters: [
+                        new Filter({
+                            path: "Name",
+                            operator: FilterOperator.Contains,
+                            value1: sSearchValue,
+                            caseSensitive: false,
+                        }),
+                        new Filter({
+                            path: "Address",
+                            operator: FilterOperator.Contains,
+                            value1: sSearchValue,
+                        }),
+                    ],
+                    or: true
+                });
+
+                if (+sSearchValue) {
+                    oFilter = new Filter({
+                        filters: [
+                            oFilter,
+                            new Filter({
+                                path: "FloorArea",
+                                operator: FilterOperator.EQ,
+                                value1: +sSearchValue
+                            }),
+                        ],
+                        or: true,
+                    })
+                }
+
+                list.getBinding("items").filter(oFilter);
+            })
+        },
 
         onListItemPress: function (oEvent) {
             const oSource = oEvent.getSource();
